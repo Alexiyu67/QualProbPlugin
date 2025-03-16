8a82856737cc6247820f9f1b55ab034024f8c0fe
Alexiyu67 <github.gurgling930@passmail.net>
2025-03-16 15:54:34 +0100

    Extending the Database

--- fr.systerel.rodinextension.sample/src/fr/systerel/rodinextension/sample/basis/IBound.java
+++ fr.systerel.rodinextension.sample/src/fr/systerel/rodinextension/sample/basis/IBound.java
@@ -4,10 +4,14 @@ import org.eventb.core.ICommentedElement;
 import org.eventb.core.IExpressionElement;
 import org.rodinp.core.IInternalElementType;
 import org.rodinp.core.RodinCore;
+import org.eclipse.core.resources.IFile;
 
 import fr.systerel.rodinextension.sample.QualProbPlugin;
 
 public interface IBound extends ICommentedElement, IExpressionElement {
 	IInternalElementType<IBound> ELEMENT_TYPE = RodinCore
 			.getInternalElementType(QualProbPlugin.PLUGIN_ID + ".bound");
+	
+	 @Override
+	    IFile getResource(); // Explicit override to resolve ambiguity
 }
